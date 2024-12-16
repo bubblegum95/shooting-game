8e7c70423559c000155564d46209609515ce39b4
Bubblegum <a82727282@gmail.com>
2024-12-16 15:56:07 +0900

    :bug: Fix: docker compose image build session

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,11 +8,11 @@ import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'postgres',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'game',
   entities: [User, Player, Match, Team],
   synchronize: true,
 });
